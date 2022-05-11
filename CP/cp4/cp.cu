a0a64d063a2e06563b050581d5722d02ba76c3da
Jenni Puhakka <jenni.puhakka@aalto.fi>
2022-05-11 14:09:09 +0300

    cp4 CUDA done

--- CP/cp4/cp.cu
+++ CP/cp4/cp.cu
@@ -2,46 +2,60 @@
 #include <cstdlib>
 #include <iostream>
 #include <cuda_runtime.h>
-#include<vector>
-using namespace std;
 
-inline void check(cudaError_t err, const char* context) 
-{
-    if (err != cudaSuccess) 
-    {
+#define BASE_VALF 0.0
+
+
+//straight copy from course material
+static inline int divup(int a, int b) {
+    return (a + b - 1)/b;
+}
+
+//straight copy from course material
+static inline void check(cudaError_t err, const char* context) {
+    if (err != cudaSuccess) {
         std::cerr << "CUDA error: " << context << ": "
             << cudaGetErrorString(err) << std::endl;
         std::exit(EXIT_FAILURE);
     }
 }
+
 #define CHECK(x) check(x, #x)
 
-inline int static divup(int a, int b) 
-{
-    return (a + b - 1)/b;
-}
-
-inline int static roundup(int a, int b) 
-{
-    return divup(a, b) * b;
-}
-
-__global__ void mykernel(int nx, int ny, const float *data, float *result) {
-    int x = threadIdx.x + blockIdx.x * blockDim.x;
-    int y = threadIdx.y + blockIdx.y * blockDim.y;
-
-    if (x >= ny || y >= ny || x < y) {    
+__global__ void mykernel(const float* data, float* result, int ny, int nx) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if (i >= ny || j >= ny)
         return;
-    }
-
-    float sum = 0.0;
-    float v = HUGE_VALF; 
-
+    float v = BASE_VALF;
     for (int k = 0; k < nx; ++k) {
-        sum += data[k + y*nx] * data[x*nx + k];
+        float x = data[nx*j + k];
+        float y = data[nx*i + k];
+        float z = x * y;
+        v += z;
     }
+    result[ny*i + j] = v;
+}
 
-    result[x + ny * y] = sum;
+//straight copy from course material
+void step(float* result, const float* d, int ny, int nx) {
+    // Allocate memory & copy data to GPU
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, ny * nx * sizeof(float)));
+    float* rGPU = NULL; 
+    CHECK(cudaMalloc((void**)&rGPU, ny * ny * sizeof(float)));
+    CHECK(cudaMemcpy(dGPU, d, ny * nx * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Run kernel
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid(divup(ny, dimBlock.x), divup(ny, dimBlock.y));
+    mykernel<<<dimGrid, dimBlock>>>(dGPU, rGPU, ny, nx);
+    CHECK(cudaGetLastError());
+
+    // Copy data back to CPU & release memory
+    CHECK(cudaMemcpy(result, rGPU, ny * ny * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
 }
 
 /*
@@ -52,55 +66,30 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
-void correlate(int ny, int nx, const float* data, float *result) 
-{
-    //preprocess data into ProcessedData
-    float * ProcessedData = (float * )calloc(nx*ny , sizeof(float));
+void correlate(int ny, int nx, const float *data, float *result) {
+    float* data_norm = (float*) malloc(ny * nx * sizeof(float));
 
-    for( int y = 0; y < ny; y++ )
-    {
-        float s = 0.0;
-        
-        for( int x = 0; x < nx; x++ )
-        {
-            s += data[ x + y * nx ];
-        }
-        s /= (float)nx;
+    for (int y = ny; y--;) {
+        double sum = 0.0, mean = 0.0, pow_sum = 0.0, sqrt_sum_sqrt = 0.0;
 
-        float t = 0.0;
-
-        for( int x = 0; x < nx; x++ )
-        {
-            ProcessedData[ x + y*nx ] = (double)data[ x + y*nx ] - s;
-            t +=  ProcessedData[ x + y * nx ] * ProcessedData[ x + y * nx ];
+        for (int x = nx; x--; ) {
+            sum += data[x + y*nx];
         }
 
-        t = sqrt( t );
+        mean = sum / nx;
 
-        for( int x = 0; x < nx; x ++ )
-        {
-            ProcessedData[ x + y * nx ] = ProcessedData[ x + y * nx ] / t;
+        for (int x = nx; x--; ) {
+            double normalized = data[x + y*nx] - mean;
+            data_norm[x + y*nx] = normalized;
+            pow_sum += normalized*normalized;
         }
-    }    
 
-    //Allocate memory and copy data to GPU
-    float * dGPU = NULL;//data for GPU
-    CHECK(cudaMalloc((void**)&dGPU, nx * ny * sizeof(float)));
+        sqrt_sum_sqrt = sqrt(pow_sum);
 
-    float * rGPU = NULL;//result for GPU
-    CHECK(cudaMalloc((void**)&rGPU, ny * ny * sizeof(float)));
+        for (int x = nx; x--; ) {
+            data_norm[x + y*nx] /= sqrt_sum_sqrt;
+        }
+    }
 
-    CHECK(cudaMemcpy(dGPU, ProcessedData, nx * ny * sizeof(float), cudaMemcpyHostToDevice));
-    
-    // Run kernel
-    dim3 dimBlock(8, 8);
-    dim3 dimGrid(roundup(ny, dimBlock.x), roundup(ny, dimBlock.y));
-    mykernel<<<dimGrid, dimBlock>>>( nx, ny, dGPU, rGPU);
-    CHECK(cudaGetLastError());
-
-    // Copy data back to CPU & release memory
-    CHECK(cudaMemcpy(result, rGPU, ny * ny * sizeof(float), cudaMemcpyDeviceToHost ));
-    CHECK(cudaFree(dGPU));
-    CHECK(cudaFree(rGPU));
-	std::free(ProcessedData);
+    step(result, data_norm, ny, nx);
 }
